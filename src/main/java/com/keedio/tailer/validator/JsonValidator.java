a62c527e62e13c17bab05c5dfdbfde0a426be051
Luca Rosellini <luca.rosellini@gmail.com>
2016-02-18 10:15:31 +0100

    Added double validation, prints an exception when line matches "^\\{.*\\}$" but is not valid for google Gson

--- src/main/java/com/keedio/tailer/validator/JsonValidator.java
+++ src/main/java/com/keedio/tailer/validator/JsonValidator.java
@@ -1,8 +1,13 @@
 package com.keedio.tailer.validator;
 
 import com.google.gson.Gson;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.regex.Pattern;
 
 /**
  * Created by luca on 15/2/16.
@@ -10,8 +15,17 @@ import org.springframework.stereotype.Component;
 @Component
 @Profile("jsonValidator")
 public class JsonValidator implements LineValidator {
+    private final static Logger LOGGER = LogManager.getLogger(JsonValidator.class);
 
     private Gson gson = new Gson();
+
+    private final String regexp = "^\\{.*\\}$";
+    private final Pattern pattern = Pattern.compile(regexp);
+
+    @PostConstruct
+    public void init(){
+        LOGGER.info("Initializing " + this.getClass().getCanonicalName());
+    }
 
     @Override
     public boolean isValid(String line) {
@@ -19,7 +33,10 @@ public class JsonValidator implements LineValidator {
             gson.fromJson(line, Object.class);
             return true;
         } catch(com.google.gson.JsonSyntaxException ex) {
-            ex.printStackTrace();
+
+            if (pattern.matcher(line).matches()){
+                LOGGER.error("Input line matches "+regexp+" but invalid for com.google.gson.Gson",ex);
+            }
             return false;
         }
     }
